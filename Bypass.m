3603b317453acf6619446b931735b7cc1ae7a998
8vz4t865m8-sudo <8vz4t865m8@privaterelay.appleid.com>
2026-06-17 21:56:43 +0800

    Update Bypass.m

--- Bypass.m
+++ Bypass.m
@@ -1,14 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#pragma mark - 干掉ShadowTrackerExtra完整性校验
 static void empty_method(id self, SEL _cmd) {}
 static id empty_return_nil(id self, SEL _cmd) { return nil; }
 static BOOL empty_return_yes(id self, SEL _cmd) { return YES; }
 
-__attribute__((constructor)) static void disable_checks() {
+// 优先级101！系统最高优先级，永远第一个执行，比ShadowTrackerExtra早100倍！
+__attribute__((constructor(101))) static void disable_checks() {
     @autoreleasepool {
-        // 1. 完全干掉ShadowTrackerExtra，所有方法都变成空函数，再也不会做任何校验
+        // 第一个执行：直接废掉ShadowTrackerExtra，它还没醒就被我们干废了
         Class Shadow = objc_getClass("ShadowTrackerExtra");
         if (Shadow) {
             unsigned int count;
@@ -25,14 +25,12 @@ __attribute__((constructor)) static void disable_checks() {
                 }
             }
             free(methods);
-            NSLog(@"✅ 完整性校验已完全禁用");
         }
         
-        // 2. 干掉T3卡密验证
+        // 然后废掉T3验证
         Class T3 = objc_getClass("T3Verify");
         if (T3) {
-            // 登录永远成功
-            class_replaceMethod(T3, @selector(loginWithKami:imei:), (IMP)imp_implementationWithBlock(^id(id self, id kami, id imei) {
+            class_replaceMethod(T3, @selector(loginWithKami:imei:), (IMP)imp_implementationWithBlock(^id(id self, id k, id i) {
                 id res = [[objc_getClass("T3LoginResult") alloc] init];
                 [res setValue:@(YES) forKey:@"success"];
                 [res setValue:@(9999999999) forKey:@"endTime"];
@@ -40,20 +38,14 @@ __attribute__((constructor)) static void disable_checks() {
                 return res;
             }), "@@:@@");
             
-            // 心跳永远成功
-            class_replaceMethod(T3, @selector(heartbeatWithKami:statecode:), (IMP)imp_implementationWithBlock(^id(id self, id kami, id code) {
+            class_replaceMethod(T3, @selector(heartbeatWithKami:statecode:), (IMP)imp_implementationWithBlock(^id(id self, id k, id c) {
                 id res = [[objc_getClass("T3Result") alloc] init];
                 [res setValue:@(YES) forKey:@"success"];
                 return res;
             }), "@@:@@");
             
-            // 初始化永远成功
             class_replaceMethod(T3, @selector(checkInit:), (IMP)empty_return_yes, "B@:^@");
-            
-            // 拦截所有网络请求
             class_replaceMethod(T3, @selector(httpPost:body:error:), (IMP)empty_return_nil, "@@:@@^@");
-            
-            NSLog(@"✅ T3验证已完全绕过");
         }
     }
 }
